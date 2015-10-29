5e2e4e02a3c92adf548cc12f1986d11866743a3a
Matt Weldon <matt.weldon@gmail.com>
2015-10-29 02:32:05 +0000

    return generated pdf from controller

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,17 @@ defmodule PdfEndpointTest.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+
+  def pdf(conn, _params) do
+    {:safe, [_ | html]} = PdfEndpointTest.PageView.render("index.html")
+    IO.inspect html
+    {:ok, tmp_pdf}
+      = %HtmlToPdf.Document{}
+        |> HtmlToPdf.set_html(html)
+        |> HtmlToPdf.generate_pdf
+
+    conn
+    |> put_resp_header("content-disposition", "attachment; filename='endpoint_test.pdf'")
+    |> send_file(200, tmp_pdf)
+  end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,6 +17,8 @@ defmodule PdfEndpointTest.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
+
+    get "/pdf", PageController, :pdf
   end
 
   # Other scopes may use custom stacks.
