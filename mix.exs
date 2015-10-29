bb361dcd700204edaa98e9844ddebabcb18c1a93
Matt Weldon <matt.weldon@gmail.com>
2015-10-29 01:58:54 +0000

    add html_to_pdf dep

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule PdfEndpointTest.Mixfile do
   def application do
     [mod: {PdfEndpointTest, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :html_to_pdf]]
   end
 
   # Specifies which paths to compile per environment
@@ -34,6 +34,7 @@ defmodule PdfEndpointTest.Mixfile do
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.1"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:html_to_pdf, "~> 0.5.0"}]
   end
 end
